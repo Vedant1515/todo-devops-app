a3c2d640fa9774d1a56bd620db475eecf913c5e0
Vedant1515 <vedant.pandya1515@gmail.com>
2025-05-21 22:29:51 +1000

    fixes

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,14 +1,17 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/app.js'; // must use .js when using ESM
+import chaiModule from 'chai';
+import chaiHttpModule from 'chai-http';
+import { expect } from 'chai';
+import app from '../src/app.js';
+
+const chai = chaiModule.default;
+const chaiHttp = chaiHttpModule.default;
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
 describe('Todo API', () => {
   it('should fetch all todos', async () => {
     const res = await chai.request(app).get('/api/todos');
     expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
   });
 });
-
